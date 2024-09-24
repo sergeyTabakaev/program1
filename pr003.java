3b2d49e3737e70d1cdc1de1aac065bf27b95a01f
sergeyTabakaev <tabakaev.ca2003@gmail.com>
2024-09-24 12:03:00 +0000

    Добавили цикл дувайл

--- pr003.java
+++ pr003.java
@@ -1,9 +1,10 @@
 class  pr003 {
 	public static void main(String args[]) throws java.io.IOException {
-		char ch;
+		char ch, ignore;
 		System.out.print("нажмите любую клавишу на клавиатуре и нажмите ENTER ");
 		ch = (char) System.in.read();
 		System.out.println("Вы нажали: " + ch);
+
 		//Угадывание буквы
                 //Пример использования проверки условия
                 char answer = 'r';
@@ -11,9 +12,11 @@ class  pr003 {
                         System.out.println("Буква указана верно");
                 else
                         System.out.println("Буква указана неверно");
+
                 //Работа с вложенными операторами if
                 System.out.println("\n");
                 int i=9, k=110, j=10, a = 5, b = 15, c = 25, d = 35;
+
                 if(i==10) {
                         if(j<20)
                                 a = b;
@@ -22,10 +25,15 @@ class  pr003 {
                         else a = c;
                 }
                 else a=d;
+
 		System.out.println("a = " + a + " c = " + c + " d = " + d);
+
 		 //Цепочка if-else-if
+
                 System.out.println("\n");
+
                 int x;
+
                 for (x=0; x<6; x++) {
                         if (x==1)
                                 System.out.println("Значение x равно 1");
@@ -38,9 +46,13 @@ class  pr003 {
                         else
                                 System.out.println("Значение х не находится между 1 и 4");
 		}
-		
+
+
+
 		 //оператор switch
+
                 System.out.println("\n");
+
                 int ii;
                 for (ii=0; ii<10; ii++)
                         switch(ii) {
@@ -62,6 +74,7 @@ class  pr003 {
                                 default:
                                         System.out.println("ii больше или равно 5");
                 }
+
 		//Пример вложенного switch
                 System.out.println("\n");
                 char ch1 = 'q', ch2 = 'h';
@@ -81,8 +94,11 @@ class  pr003 {
                                 System.out.println("Не выведется");
                                 break;
                 }
+
 		//Оператор цикла
+
 		System.out.println("\n");
+
 		double num, sroot, rerr;
 
 		for(num = 1; num < 100; num++){
@@ -103,11 +119,15 @@ class  pr003 {
 		for (o7 = 0, j7 = 10; o7<j7; o7++,j7--){
 			System.out.println("o7 = " + o7 + " j7 = " + j7);
 		}
+
 		int i8;
 		System.out.println("Для остановки цикла нажмите клавишу 'S' ");
-		for(i8=0;(char)System.in.read()!='S';i8++){
-			System.out.println("Иттерация № " + i8);
+		for(i8=0;(char)System.in.read()!='S';i++){
+			do{
+                                ignore = (char)System.in.read();
+                        }while(ignore!= '\n' );
 		}
+
 
 		for(i8 = 0; i8 < 10;){
 			System.out.println("Иттерация № " + i8);
@@ -118,22 +138,23 @@ class  pr003 {
                         System.out.println("Иттерация № " + i8);
                         i8++;
                 }
-		
+
 		//Бесконечный цикл
 		//for(;;);
-		
+
 		int sum = 0;
 		for(i8 = 1; i8 <= 5;sum+=i8++);
 		System.out.println("Сумма чисел равна: " + sum);
 
 		//while
-		
+
 		ch = 'а';
 		while(ch<='я'){
 			System.out.print(ch);
 			ch++;
 		}
 		System.out.println();
+
 		int e, result;
 		for(int i9 = 0 ; i9<10;i9++){
 			result = 1;
@@ -144,5 +165,15 @@ class  pr003 {
 			}
 			System.out.println("Два в степени " + i9 + " равно " + result);
 		}
+		//Цикл ду-вайл
+		//
+	
+		do {
+			System.out.print("Нажмите клавишу, затем ENTER: ");
+			ch = (char)System.in.read();
+			do{
+				ignore = (char)System.in.read();
+			}while(ignore!= '\n' );
+		}while(ch!='q');
 	}
 }
